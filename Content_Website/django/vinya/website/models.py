3bd46210a60132605bee5d470efc7c0fc8a225a1
Kenan Rhoton <kenan.rhoton@gmail.com>
2016-06-01 18:20:14 +0200

    Added Sections

--- Content_Website/django/vinya/website/models.py
+++ Content_Website/django/vinya/website/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Section(models.Model):
+    name = models.CharField(max_length=200)
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    sections = models.ManyToManyField(Section)
+
+    def addSection(self, s):
+        self.sections.add(s)
+        self.save()
--- Content_Website/django/vinya/website/tests.py
+++ Content_Website/django/vinya/website/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 from django.core.urlresolvers import reverse
 
-from .models import Post
+from .models import Post, Section
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
@@ -28,3 +28,34 @@ class UserTests(TestCase):
 
     def test_user_cant_be_authenticated_if_it_doesnt_exists(self):
         self.assertIsNone(authenticate(username='paco', password='flanesyqueso'))
+
+
+class SectionTests(TestCase):
+
+    def test_post_starts_without_sections(self):
+        new_post = Post(title = "Mah title", content = "Mah content")
+        new_post.save()
+        self.assertEqual(0,new_post.sections.count())
+
+    def test_post_can_have_a_section(self):
+        new_post = Post(title = "Mah title", content = "Mah content")
+        new_post.save()
+        sec = Section(name="Potatoes")
+        sec.save()
+        new_post.addSection(sec)
+        self.assertEqual(1,new_post.sections.count())
+
+#TODO
+class BlockTests(TestCase):
+
+    def test_sections_can_be_on_a_block(self):
+        pass
+
+    def test_blocks_can_be_positioned_right(self):
+        pass
+
+    def test_blocks_can_be_positioned_left(self):
+        pass
+
+    def test_blocks_can_be_positioned_center(self):
+        pass
